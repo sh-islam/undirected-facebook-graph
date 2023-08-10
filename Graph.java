123a612304bc9b9be598d4643986d8fee2e38279
sh-islam <misla2@uwo.ca>
2023-08-10 17:16:13 -0400

    Matrix and suggestFriends() in O(n^2) time

--- Graph.java
+++ Graph.java
@@ -1,10 +1,12 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 
 public class Graph {
     int[][] matrix;
-    ArrayList<Node> nodes; // Reimplement this with hashmap to get O(c) for checkFriends()
+    ArrayList<Node> nodes;
 
     Graph (int size){
         nodes = new ArrayList<>();
@@ -24,14 +26,13 @@ public class Graph {
     public void generateFriendMatrix(){
         int size = nodes.size();
         this.matrix = new int[size][size];
-        // O(n^3) complexity
+        // O(n^2) complexity
         for (int i = 0; i < nodes.size(); i++){
             Node currentNode = nodes.get(i);
             for (int j = 0; j < nodes.size(); j++){
                 Node otherNode = nodes.get(j);
-                // If hashmap: if (currentNode.friends.get(otherNode) != null) add the edge
-                // This means otherNode exists in the friends hashmap of currentNode
-                if (currentNode.checkIfFriend(otherNode)){  //checkIfFriend is O(n) if hashmap is used, the get(k) of hashmap is O(c)
+                // if otherNode is a friend of a current node, add an edge
+                if (currentNode.checkIfFriend(otherNode)){  //checkIfFriend is O(1) since HashSet is used
                     this.matrix[i][j] = 1;
                 }
             }
@@ -67,64 +68,50 @@ public class Graph {
         else return false; // Total time complexity: O(n)
     }
 
-    // Returns an array of friends of friends that user is not friends with
-    public ArrayList suggestFriends(Node node){
-        ArrayList suggestedFriends = new ArrayList<>();
-
-        // O(n^3)
-        // Adding friends of friends, if they are not the same person needing suggestions, or if not already suggested
-        for (int i = 0; i < node.friends.size(); i++){
-            Node currentFriend = (Node) node.friends.get(i);
-
-            for (int j = 0; j < currentFriend.friends.size(); j++){
-                Node friendsFriend = (Node) currentFriend.friends.get(j);
-                // If: O(c) + O(n) + O(n) = O(n)
-                if (friendsFriend != node && !suggestedFriends.contains(friendsFriend) && checkEdge(node, friendsFriend) == false) {
+    // Returns a of friends of friends that user is not friends with
+    public Set<Node> suggestFriends(Node node){
+        Set<Node> suggestedFriends = new HashSet<>();
+        // Total time complexity: O(n^2), two nested for loops with O(1) complexity in the inner loop
+        for (Node friend : node.friends){
+            for (Node friendsFriend : friend.friends){
+                // O(1) + O(1) + O(1)
+                if (friendsFriend != node & !suggestedFriends.contains(friendsFriend) && node.checkIfFriend(friendsFriend) == false){
                     suggestedFriends.add(friendsFriend);
                 }
             }
-
         }
         System.out.println("Suggested friends for " + node.username + ":");
         printSuggestedFriends(suggestedFriends);
         return suggestedFriends;
     }
 
-    public void printSuggestedFriends (ArrayList suggestedFriends){
-        for (int i = 0; i < suggestedFriends.size(); i++){
-            Node currentSuggestedFriend = (Node) suggestedFriends.get(i);
-            System.out.print(currentSuggestedFriend.username + " ");
-        }
+    public void printSuggestedFriends (Set<Node> suggestedFriends){
+        for (Node node : suggestedFriends) System.out.print(node.username + " ");
+        System.out.println("");
     }
 
 }
 
 class Node{
     String username;
-    ArrayList friends;
+    Set<Node> friends;
 
     Node (String name){
         username = name;
-        friends = new ArrayList<Node>();
+        friends = new HashSet<>();
     }
 
     void addFriends(Node[] friends){
         this.friends.addAll(Arrays.asList(friends)); // O(n) bc friends is an arraylist
     }
 
-    boolean checkIfFriend(Node node){
-        for (int i = 0; i < this.friends.size(); i++){
-            Node currentFriend = (Node) this.friends.get(i);
-            if (node == currentFriend) return true;
-        }
+    boolean checkIfFriend(Node node) {
+        if (this.friends.contains(node)) return true; // Hashset used for friends, so O(1) average for contains op
         return false;
     }
 
-    void printFriends(){
-        for (int i = 0; i < friends.size(); i++){
-            Node currentFriend = (Node) friends.get(i);
-            System.out.print(currentFriend.username + " ");
-        }
+    void printFriends() {
+        for (Node friend : friends) System.out.print(friend.username + " ");
         System.out.println("");
     }
 
@@ -149,7 +136,5 @@ class Node{
         graph.generateFriendMatrix();
         graph.printFriendMatrix();
         graph.suggestFriends(E);
-
-
     }
 }
