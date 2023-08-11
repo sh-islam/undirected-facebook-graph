1067cad191eb2d7f878741ade1c66c96ec0c6973
sh-islam <misla2@uwo.ca>
2023-08-11 16:17:32 -0400

    Cleaned up node class, added edges and Dijkstra's algorithm

--- Graph.java
+++ Graph.java
@@ -1,140 +1,164 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
+
+class Node {
+    String username;
+
+    Node (String name){
+        this.username = name;
+    }
+}
 
 
 public class Graph {
-    int[][] matrix;
-    ArrayList<Node> nodes;
 
-    Graph (int size){
-        nodes = new ArrayList<>();
-        matrix = new int[size][size];
-    }
+   int [][] distanceMatrix;
+   Set<Node> nodes = new HashSet<>();
+   Map<Node, Integer> nodeIndices = new HashMap<>();
 
+   // O(n)
+   public void addNodes(Node [] nodes){
+       this.nodes.addAll(Arrays.asList(nodes)); // O(n)
+       // Storing nodes and indices
+       int index = 0;
+       for (Node node: this.nodes){
+           nodeIndices.put(node, index);    // O(n)
+           index++;
+       }
 
-    public void addNodes (Node[] nodes){
-        for (Node node : nodes){
-            addNode(node);
-        }
-    }
-    public void addNode (Node node){
-        nodes.add(node);
-    }
+       generateMatrix();    // Initializing matrix takes O(n^2) time
+   }
 
-    public void generateFriendMatrix(){
+   // O(1)
+   public void addEdges(Node node1, Node node2, int distance){
+       int index1 = nodeIndices.get(node1);
+       int index2 = nodeIndices.get(node2);
+       distanceMatrix[index1][index2] = distance;
+       distanceMatrix[index2][index1] = distance;   // Undirected graph, also adding edge for other node
+   }
+
+    // O(n^2)
+    private void generateMatrix() {
         int size = nodes.size();
-        this.matrix = new int[size][size];
-        // O(n^2) complexity
-        for (int i = 0; i < nodes.size(); i++){
-            Node currentNode = nodes.get(i);
-            for (int j = 0; j < nodes.size(); j++){
-                Node otherNode = nodes.get(j);
-                // if otherNode is a friend of a current node, add an edge
-                if (currentNode.checkIfFriend(otherNode)){  //checkIfFriend is O(1) since HashSet is used
-                    this.matrix[i][j] = 1;
+        distanceMatrix = new int[size][size];
+        for (int[] row : distanceMatrix) Arrays.fill(row, 0); // Init each row with 0 (no edge)
+    }
+
+    // O(n^2)
+    private void printMatrix(){
+       for (Node node : nodes) System.out.print(node.username.charAt(0) + " ");
+       System.out.println("");
+
+       for (int i = 0; i < nodes.size(); i++){
+           for (int j = 0; j < nodes.size(); j++){
+               System.out.print(distanceMatrix[i][j] + " ");
+           }
+           System.out.println("");
+       }
+    }
+
+    public ArrayList<Node> suggestFriends(Node rootNode){
+       ArrayList<Node> suggestedFriends = new ArrayList<>();
+       ArrayList<Node> currentFriends = new ArrayList<>();
+       ArrayList<Node> prevFriends = new ArrayList<>(); // To store the friend of the suggested friend
+       int rootNodeIndex = nodeIndices.get(rootNode);
+
+       // Search through nodes to find which nodes share an edge with given rootNode O(n)
+       for (Node node : nodes){
+           int nodeIndex = nodeIndices.get(node);
+           if (distanceMatrix[rootNodeIndex][nodeIndex] != 0) {
+               currentFriends.add(node);
+           }
+       }
+
+       // Search through currentFriends to find nodes that it has edges with but no edges with the rootNode
+        for (Node friend : currentFriends){
+            int friendIndex = nodeIndices.get(friend);
+
+            for (Node node: nodes){
+                if (node == rootNode) break;
+                int nodeIndex = nodeIndices.get(node);
+                if (distanceMatrix[friendIndex][nodeIndex] != 0 && distanceMatrix[rootNodeIndex][nodeIndex] == 0){
+                    suggestedFriends.add(node);
+                    prevFriends.add(friend);
                 }
             }
         }
-    }
 
-    public void printFriendMatrix(){
-        for (int i = 0; i < nodes.size(); i ++){
-            System.out.print((nodes.get(i).username).charAt(0) + " ");
+        // Printing suggested friends
+        System.out.println("Suggested friends for " + rootNode.username + ":");
+        for (int i = 0; i < suggestedFriends.size(); i ++){
+            System.out.println(suggestedFriends.get(i).username + " b/c " + rootNode.username + " is friends with " + prevFriends.get(i).username);
         }
-        System.out.println("");
 
-        for (int i =0; i < nodes.size(); i++){
-            for (int j = 0; j < nodes.size(); j++){
-                System.out.print(matrix[i][j] + " ");
-            }
-            System.out.println("");
-        }
-    }
-
-    private int getIndex(Node node){
-        int index;
-        for (index = 0; index < nodes.size(); index++){
-            if (node == nodes.get(index)) return index;
-        }
-        return -1;
-    }
-
-    public boolean checkEdge(Node node1, Node node2){
-        int index1 = getIndex(node1); // O(n)
-        int index2 = getIndex(node2); // O(n)
-        if (this.matrix[index1][index2] == 1) return true; // O(c)
-        else return false; // Total time complexity: O(n)
-    }
-
-    // Returns a of friends of friends that user is not friends with
-    public Set<Node> suggestFriends(Node node){
-        Set<Node> suggestedFriends = new HashSet<>();
-        // Total time complexity: O(n^2), two nested for loops with O(1) complexity in the inner loop
-        for (Node friend : node.friends){
-            for (Node friendsFriend : friend.friends){
-                // O(1) + O(1) + O(1)
-                if (friendsFriend != node & !suggestedFriends.contains(friendsFriend) && node.checkIfFriend(friendsFriend) == false){
-                    suggestedFriends.add(friendsFriend);
-                }
-            }
-        }
-        System.out.println("Suggested friends for " + node.username + ":");
-        printSuggestedFriends(suggestedFriends);
         return suggestedFriends;
     }
 
-    public void printSuggestedFriends (Set<Node> suggestedFriends){
-        for (Node node : suggestedFriends) System.out.print(node.username + " ");
-        System.out.println("");
+    // Dijkstra's algorithm O(n^2)
+    public void shortestPath(Node rootNode){
+       boolean[] visitedArr = new boolean[nodes.size()];
+       int[] distanceArr = new int[nodes.size()];
+       Node [] prevNode = new Node[nodes.size()];
+       Node[] nodeArr = nodes.toArray(new Node[0]);
+
+       // Init all distances to be infinity, except the first/root node
+        for (int i = 0; i < nodes.size(); i++) distanceArr[i] = Integer.MAX_VALUE;
+        int rootIndex = nodeIndices.get(rootNode);
+        distanceArr[rootIndex] = 0;
+
+        for (int i = 0; i < nodes.size(); i++){
+            int minDistance = Integer.MAX_VALUE;
+            int minIndex = -1;
+
+            // Find node with the smallest distance that has not been visited
+            for (int j = 0; j < nodes.size(); j++){
+                if (distanceArr[j] < minDistance && !visitedArr[j]){
+                    minDistance = distanceArr[j];
+                    minIndex = j;
+                }
+            }
+
+            visitedArr[minIndex] = true; // Marking as visited
+            if (minIndex == -1) break; // No edges found, no other nodes to visit
+
+            // Find distance to neighbouring nodes from prev node with the smallest distance
+            // and update the shortest distance to that node
+            for (int j = 0; j < nodes.size(); j++){
+                if (distanceMatrix[minIndex][j] != 0 && !visitedArr[j]){
+                    int newDistance = minDistance + distanceMatrix[minIndex][j];
+                    if (newDistance < distanceArr[j]) {
+                        distanceArr[j] = newDistance;
+                        prevNode[j] = nodeArr[minIndex];
+                    }
+                }
+            }
+        }
+
+        // Print out shortest node:
+        for (int i =0; i < nodes.size(); i ++){
+            String otherUsername = nodeArr[i].username;
+            int distance = distanceArr[i];
+            String prevNodeUsername = rootNode.username;
+            if (prevNode[i] != null) prevNodeUsername = prevNode[i].username;
+            System.out.println(rootNode.username + " to " + otherUsername + " " + distance + ". Previous vertex: " + prevNodeUsername);
+        }
     }
-
-}
-
-class Node{
-    String username;
-    Set<Node> friends;
-
-    Node (String name){
-        username = name;
-        friends = new HashSet<>();
-    }
-
-    void addFriends(Node[] friends){
-        this.friends.addAll(Arrays.asList(friends)); // O(n) bc friends is an arraylist
-    }
-
-    boolean checkIfFriend(Node node) {
-        if (this.friends.contains(node)) return true; // Hashset used for friends, so O(1) average for contains op
-        return false;
-    }
-
-    void printFriends() {
-        for (Node friend : friends) System.out.print(friend.username + " ");
-        System.out.println("");
-    }
-
 
     public static void main(String[] args) {
-        Node A = new Node("ryat");
-        Node B = new Node("laura");
-        Node C = new Node("taufiq");
-        Node D = new Node("bejoy");
-        Node E = new Node("taqrim");
-        Node F = new Node("abrar");
+        Node A = new Node("Alpha");
+        Node B = new Node("Bravo");
+        Node C = new Node("Charlie");
+        Node D = new Node("Delta");
 
-        A.addFriends(new Node[]{B, C, D, E});
-        B.addFriends(new Node[] {A, C, D, E});
-        C.addFriends(new Node[] {A, B, E});
-        D.addFriends(new Node[] {A,B});
-        E.addFriends(new Node[]{A, B, C,F});
-        F.addFriends(new Node[] {E});
+        Graph graph = new Graph();
+        graph.addNodes(new Node[]{A,B,C,D});
+        graph.addEdges(A, B, 1);
+        graph.addEdges(A, C, 2);
+        graph.addEdges(B, D, 4);
+        graph.addEdges(C, D, 2);
 
-        Graph graph = new Graph(6);
-        graph.addNodes(new Node[] {A,B,C,D,E,F});
-        graph.generateFriendMatrix();
-        graph.printFriendMatrix();
-        graph.suggestFriends(E);
+        graph.printMatrix();
+        graph.shortestPath(A);
+        graph.suggestFriends(A);
+
     }
 }
